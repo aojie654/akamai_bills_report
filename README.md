e5cb7bf1623784db5f899fbdf0009d70e6a377de
aojie654 <aojie654@live.cn>
2019-11-21 11:01:50 +0800

    Init repo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # akamai_bills_report
+
+## Usage
+
+用来合并多个Akamai Traffic Report
---
+++ geo/merge.py
@@ -0,0 +1,15 @@
+# encoding: utf-8 
+'''
+@File    :   merge.py
+@Time    :   2019/11/20 20:01:16
+@Author  :   aojie654
+@Contact :   aojie654@live.cn
+@Desc    :   Merge the GEO traffic report of Akamai
+'''
+
+import os, datetime
+
+csv_path='/Volumes/data/tmp/downloads/tmp/'
+DSA_path='DSA.csv'
+DSD_path='DSD.csv'
+result_path='result.csv'
