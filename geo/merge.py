c817758806bc122945ef67707dac67dd3704e2b5
aojie654 <aojie654@live.cn>
2019-11-28 10:38:01 +0800

    1. Add defines of csv_path, customer_name, files_list, result_path 2. Add defines of functions merge_csv_content, read_csv_content, save_csv_content 3. Update the function main 4. Add comments

--- geo/merge.py
+++ geo/merge.py
@@ -1,4 +1,4 @@
-# encoding: utf-8 
+# encoding: utf-8
 '''
 @File    :   merge.py
 @Time    :   2019/11/20 20:01:16
@@ -7,9 +7,53 @@
 @Desc    :   Merge the GEO traffic report of Akamai
 '''
 
-import os, datetime
+'''
+Notes:
+1. Billing tittle in line 20 and data begin from line 24 in csv's content
+'''
 
-csv_path='/Volumes/data/tmp/downloads/tmp/'
-DSA_path='DSA.csv'
-DSD_path='DSD.csv'
-result_path='result.csv'
+
+# get the date
+import csv
+import time
+import os
+cur_date = time.strftime("%Y-%m-%d", time.gmtime())
+
+# define the path of files location
+csv_path = '/Volumes/data/tmp/downloads/akamai_bills_geo'
+# define the name of customer
+customer_name = input('Input the name of customer:')
+
+# name of files need to merge
+files_list = ['DSA.csv', 'DSD.csv']
+# name of final result
+result_path = customer_name + 'bills'
+
+
+def merge_csv_content(*args):
+    '''
+    define the function that merge the content
+    '''
+    return 0
+
+
+def read_csv_content(csv_file):
+    '''
+    define the function that read the csv files
+    '''
+    return 0
+
+
+def save_csv_content(*args):
+    '''
+    define the function that save the content to final result file
+    '''
+    return 0
+
+
+if __name__ == "__main__":
+    # change the work dir to $csv_path
+    os.chdir(csv_path)
+    # merge the csv content
+    merged_result = merge_csv_content(files_list)
+    save_csv_content(merged_result)
