fb6a1c172345e928e148a62836a5fbef4f6ec861
aojie654 <aojie654@live.cn>
2019-11-28 11:18:32 +0800

    Create the function of get files name list

--- geo/merge.py
+++ geo/merge.py
@@ -12,48 +12,69 @@ Notes:
 1. Billing tittle in line 20 and data begin from line 24 in csv's content
 '''
 
-
-# get the date
+# Import the libs
+# The following pass is using to avoid the formate issue
 import csv
 import time
 import os
+import pathlib
+pass
+
+# Get the current date
 cur_date = time.strftime("%Y-%m-%d", time.gmtime())
 
-# define the path of files location
-csv_path = '/Volumes/data/tmp/downloads/akamai_bills_geo'
-# define the name of customer
+# Define the dir of files location
+dir_path = '/Volumes/data/tmp/downloads/akamai_bills_geo'
+# Define the name of customer
 customer_name = input('Input the name of customer:')
 
-# name of files need to merge
-files_list = ['DSA.csv', 'DSD.csv']
-# name of final result
-result_path = customer_name + 'bills'
+# Name of files need to merge
+files_list = get_file_list_name(dir_path)
+# Name of final result
+result_path = customer_name + '-bills-' + cur_date + '.csv'
 
+def get_file_list_name(dir_path):
+    '''
+    Define the function to get the name of csv files
+    '''
+    # Set the current path to pathlib.Path
+    path_object = pathlib.Path('.')
+    # Get the object of files list
+    files_object = path_object.glob("*.csv")
+    # Put the files objects to list
+    files_object_list = list(files_object)
+    # convert the files_object_list to strings and store them to a new list
+    files_name_list = []
+    for file_object in files_object_list:
+        file_name = file_object.name
+        files_name_list.append(file_name)
+
+    return files_name_list
 
 def merge_csv_content(*args):
     '''
-    define the function that merge the content
+    Define the function that merge the content
     '''
     return 0
 
 
 def read_csv_content(csv_file):
     '''
-    define the function that read the csv files
+    Define the function that read the csv files
     '''
     return 0
 
 
 def save_csv_content(*args):
     '''
-    define the function that save the content to final result file
+    Define the function that save the content to final result file
     '''
     return 0
 
 
 if __name__ == "__main__":
-    # change the work dir to $csv_path
-    os.chdir(csv_path)
-    # merge the csv content
+    # Change the work dir to $dir_path
+    os.chdir(dir_path)
+    # Merge the csv content
     merged_result = merge_csv_content(files_list)
     save_csv_content(merged_result)
